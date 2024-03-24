db621e3864133e54e0b9e996e698c1f07c4fffd7
Kurucz Laci <lacikurucz09@gmail.com>
2024-03-24 01:57:26 +0100

    php teszt

--- gyak6/index.php
+++ gyak6/index.php
@@ -1,23 +1,38 @@
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title>Document</title>
 </head>
+
 <body>
-    
+
     <?php
-    
 
-        echo phpversion();
 
-        
+    echo phpversion();
 
-    
+
     ?>
+
+
+    <form action="welcome.php" method="POST" style="margin: auto;">
+
+
+        <input type="text" name="var">
+
+        <input type="submit" value="Küldés">
+
+
+
+
+    </form>
+
 
 
 
 
 </body>
+
 </html>
